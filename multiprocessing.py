b8e0149438743facce16dacad22f6bf6e2a9379f
Julian <julian@Ingeneo-MacBook-Pro.local>
2019-10-08 08:18:50 -0500

    Learning multiprocessing in python

---
+++ multiprocessing.py
@@ -0,0 +1,24 @@
+import time
+import threading
+import multiprocessing
+
+def countdown(number):
+    while number > 0:
+        number -= 1
+
+if __name__ == '__main__':
+    start = time.time()
+
+    count = 100000000
+
+    t1 = multiprocessing.Process(target=countdown, args=(count,))
+    t2 = multiprocessing.Process(target=countdown, args=(count,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print(f'tiempo transcurrido {time.time() - start}')
+    
